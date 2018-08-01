ebc1dbc2e4a6f6e61be164c7ba93bcdd064f07fb
Cabezas <javiergonzalezcabezas@gmail.com>
2018-08-01 22:49:32 +0100

    remove stack in DetailFragment

--- app/src/main/java/com/thedeveloperworldisyours/championsaffinity/detail/DetailActivity.kt
+++ app/src/main/java/com/thedeveloperworldisyours/championsaffinity/detail/DetailActivity.kt
@@ -1,6 +1,7 @@
 package com.thedeveloperworldisyours.championsaffinity.detail
 
 import android.os.Bundle
+import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
 import com.thedeveloperworldisyours.championsaffinity.R
 
@@ -12,5 +13,17 @@ class DetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.detail_activity)
+        if (savedInstanceState == null) {
+            changeFragment(DetailFragment())
+        }
     }
+
+    fun changeFragment(f: Fragment, cleanStack: Boolean = false) {
+        val ft = supportFragmentManager.beginTransaction()
+        ft.setCustomAnimations(
+                R.anim.abc_fade_in, R.anim.abc_fade_out, R.anim.abc_popup_enter, R.anim.abc_popup_exit)
+        ft.add(R.id.detail_activity_contentFrame, f)
+        ft.commit()
+    }
+
 }
--- app/src/main/java/com/thedeveloperworldisyours/championsaffinity/detail/DetailFragment.kt
+++ app/src/main/java/com/thedeveloperworldisyours/championsaffinity/detail/DetailFragment.kt
@@ -12,9 +12,6 @@ import com.thedeveloperworldisyours.championsaffinity.R
  * Created by javiergonzalezcabezas on 30/7/18.
  */
 class DetailFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater?.inflate(R.layout.detail_fragment, container, false)
